89bf02655a17de72dbdd7fdc8772e0dec4e37b98
Call17999 <joao.crespim.ferrari@escola.pr.gov.br>
2026-02-26 15:06:13 -0300

    Add files via upload

---
+++ perimetro_triangulo.cpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+cout<< "calcule o perimetro do triangulo:";
+cout<< "\n---------------------------------";
+    // Tipo inteiro
+    
+    int a;
+    int b;
+    int c;
+    
+     cout<< "\nDigite o valor do lado a:";
+    cin>> a;
+    
+     cout<< "\nDigite o valor do lado b:";
+    cin>> b;
+    
+     cout<< "\nDigite o valor do lado c:";
+    cin>> c;
+    
+    int soma = a+b+c;
+    
+    cout << "\nSoma = "<< soma;
+    
+    return 0;
+    
+}
