075e1be19a311e61a7e61da39be25280c113c294
Call17999 <joao.crespim.ferrari@escola.pr.gov.br>
2026-03-05 17:36:12 -0300

    Add files via upload

---
+++ logica bool cont.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+int main();
+{
+    
+    bool p = 1;
+    bool q = 0;
+    
+    cout<<"Condicional";
+    bool cond = !p || q;
+    
+    cout<< "Valor de se p então q: "<< cond;
+    cout<< "\n--------------------" << cond;
+    
+    cout<<"bicondicional";
+    bool bicond = !(p||q) || (p&&q);
+    cout<<"valor de p se, e somente se q:"<<;
+    cout<<"\n---------------------------";
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
