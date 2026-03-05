efb12c10c3f21b1419f8e5422efeb7c6ef4d16ac
Call17999 <joao.crespim.ferrari@escola.pr.gov.br>
2026-03-05 15:15:34 -0300

    Add files via upload

---
+++ media notas.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    double n1, n2, n3, ma;
+    
+    cout<<"\ninsira a nota do primeiro trimestre: ";
+    cin>>n1;
+    
+    cout<<"\ninsira a nota do segundo trimestre: ";
+    cin>>n2;
+    
+    cout<<"\ninsira a nota do terceiro trimestre: ";
+    cin>>n3;
+    
+    ma = (n1+n2+n3)/3;
+    cout<<"Media: " << ma;
+    
+    
+    
+    cout<<"Hello World";
+
+    return 0;
+}
